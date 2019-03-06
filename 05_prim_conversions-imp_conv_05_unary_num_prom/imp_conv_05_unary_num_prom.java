5a9d30b9d5854958d8ac7ebf846b78d10fe8492a
Pengyu Nie <prodigy.sov@gmail.com>
2019-03-06 13:15:26 -0600

    Manually separating a print.

--- 05_prim_conversions-imp_conv_05_unary_num_prom/imp_conv_05_unary_num_prom.java
+++ 05_prim_conversions-imp_conv_05_unary_num_prom/imp_conv_05_unary_num_prom.java
@@ -8,20 +8,20 @@ Unary numeric promotion:
 */
 public class imp_conv_05_unary_num_prom {
   public static void main(String[] args) {
-    System.out.println("" + (+(short)1000) + " " +(-(short)(-32768))
-      + " " +((short)(-32768)));
-    System.out.println(""
-      + ((short)10000 >> (short)2) + " "
-      + ((short)10000 << (short)2) + " "
-      + ((short)-10000 >>> (short)2) );
-    System.out.println(""
-      + (10000L >> (short)2) + " "
-      + (10000L << (short)24) + " "
-      + (-10000L >>> (short)2) );
-      System.out.println(""
-      + ((short)10000 >> 2L) + " "
-      + ((short)10001 << 24L) + " "
-      + ((short)-10000 >>> 2L) );
+      System.out.print(""); System.out.print((+(short)1000)); System.out.print(" "); System.out.print((-(short)(-32768)));
+      System.out.print(" "); System.out.println(((short)(-32768)));
+      System.out.print("");
+      System.out.print(((short)10000 >> (short)2)); System.out.print(" ");
+      System.out.print(((short)10000 << (short)2)); System.out.print(" ");
+      System.out.println(((short)-10000 >>> (short)2) );
+      System.out.print("");
+      System.out.print((10000L >> (short)2)); System.out.print(" ");
+      System.out.print((10000L << (short)24)); System.out.print(" ");
+      System.out.println((-10000L >>> (short)2) );
+      System.out.print("");
+      System.out.print(((short)10000 >> 2L)); System.out.print(" ");
+      System.out.print(((short)10001 << 24L)); System.out.print(" ");
+      System.out.println(((short)-10000 >>> 2L) );
       System.out.println("Done!");
   }
 }
