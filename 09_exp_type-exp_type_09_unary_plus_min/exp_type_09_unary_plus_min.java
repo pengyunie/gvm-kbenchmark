c32795fc1bf8ccecf8efd441c386bdde775adf3b
Pengyu Nie <prodigy.sov@gmail.com>
2019-03-06 13:22:31 -0600

    Protect some addition in print.

--- 09_exp_type-exp_type_09_unary_plus_min/exp_type_09_unary_plus_min.java
+++ 09_exp_type-exp_type_09_unary_plus_min/exp_type_09_unary_plus_min.java
@@ -7,10 +7,10 @@ int b = 1; long a = 10;
 
 public class exp_type_09_unary_plus_min {
   public static void main(String[] args) {
-    System.out.println("f(true  ? +get(1) : +getL(10)): " + f(true  ? +get(1) : +getL(10)));
-    System.out.println("f(false ? +get(1) : +getL(10)): " + f(false ? +get(1) : +getL(10)));
-    System.out.println("f(true  ? -get(1) : -getL(10)): " + f(true  ? -get(1) : -getL(10)));
-    System.out.println("f(false ? -get(1) : -getL(10)): " + f(false ? -get(1) : -getL(10)));
+      System.out.println("f(true  ? +get(1) : +getL(10)): " + (f(true  ? +get(1) : +getL(10))));
+      System.out.println("f(false ? +get(1) : +getL(10)): " + (f(false ? +get(1) : +getL(10))));
+      System.out.println("f(true  ? -get(1) : -getL(10)): " + (f(true  ? -get(1) : -getL(10))));
+      System.out.println("f(false ? -get(1) : -getL(10)): " + (f(false ? -get(1) : -getL(10))));
 
     System.out.println("Done!");
   }
--- 09_exp_type-exp_type_15_int_add/exp_type_15_int_add.java
+++ 09_exp_type-exp_type_15_int_add/exp_type_15_int_add.java
@@ -7,10 +7,10 @@ b - b : a - b
 
 public class exp_type_15_int_add {
   public static void main(String[] args) {
-    System.out.println("f(true  ? get(10) + 10 : getL(100) + 10): " + f(true  ? get(10) + 10 : getL(100) + 10));
-    System.out.println("f(false ? get(10) + 10 : getL(100) + 10): " + f(false ? get(10) + 10 : getL(100) + 10));
-    System.out.println("f(true  ? get(10) - 10 : getL(100) - 10): " + f(true  ? get(10) - 10 : getL(100) - 10));
-    System.out.println("f(false ? get(10) - 10 : getL(100) - 10): " + f(false ? get(10) - 10 : getL(100) - 10));
+      System.out.println("f(true  ? get(10) + 10 : getL(100) + 10): " + (f(true  ? get(10) + 10 : getL(100) + 10)));
+      System.out.println("f(false ? get(10) + 10 : getL(100) + 10): " + (f(false ? get(10) + 10 : getL(100) + 10)));
+      System.out.println("f(true  ? get(10) - 10 : getL(100) - 10): " + (f(true  ? get(10) - 10 : getL(100) - 10)));
+      System.out.println("f(false ? get(10) - 10 : getL(100) - 10): " + (f(false ? get(10) - 10 : getL(100) - 10)));
 
     System.out.println("Done!");
   }
